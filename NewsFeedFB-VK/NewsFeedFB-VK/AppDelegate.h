9f50a8bb8cdbd4ac387ecca472e807f631d9b89c
Viktoriia Vovk <zzww13wwzz@gmail.com>
2017-03-28 19:31:10 +0300

    fist comit

---
+++ NewsFeedFB-VK/NewsFeedFB-VK/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  NewsFeedFB-VK
+//
+//  Created by Viktoriia Vovk on 3/28/17.
+//  Copyright © 2017 Viktoriia Vovk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ NewsFeedFB-VK/NewsFeedFB-VK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NewsFeedFB-VK
+//
+//  Created by Viktoriia Vovk on 3/28/17.
+//  Copyright © 2017 Viktoriia Vovk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
